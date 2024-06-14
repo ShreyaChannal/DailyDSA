7f88207e8db167a96bac29d2d441fe45f0f2cca5
ShreyaChannal <shreyachannal2003@gmail.com>
2024-06-14 13:37:08 +0530

    created new graph

--- Graphs.java
+++ Graphs.java
@@ -4,12 +4,12 @@ public class Graphs{
     static class Edge{
         int src;
         int des;
-        int wt;
+        //int wt;
 
-        public Edge(int s,int d,int w){
+        public Edge(int s,int d){
             this.src=s;
             this.des=d;
-            this.wt=w;
+            //this.wt=w;
         }
     }
 
@@ -18,9 +18,28 @@ public class Graphs{
             graph[i]=new ArrayList<Edge>(); //this creates empty arraylists at that position in the array
         }
 
-        graph[0].add(new Edge(0,2,1));
-        graph[1].add(new Edge(1,2,2));
-        graph[1].add(new Edge (1,3,3));
+        graph[0].add(new Edge(0,2));
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1,0));
+        graph[1].add(new Edge (1,3));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
+
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+        graph[5].add(new Edge(5, 6));
+
+        graph[6].add(new Edge(6, 5));
        
 
     }
